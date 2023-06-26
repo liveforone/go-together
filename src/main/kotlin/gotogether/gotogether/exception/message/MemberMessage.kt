b6469e1203bbcec053f86e00c5b84555a547a5f4
liveforone <yc4852@gmail.com>
2023-06-26 15:26:36 +0900

    Add member is null message.

--- src/main/kotlin/gotogether/gotogether/exception/message/MemberMessage.kt
+++ src/main/kotlin/gotogether/gotogether/exception/message/MemberMessage.kt
@@ -1,5 +1,5 @@
 package gotogether.gotogether.exception.message
 
 enum class MemberMessage(val message: String) {
-    WRONG_PASSWORD("비밀번호를 틀렸습니다.")
+    WRONG_PASSWORD("비밀번호를 틀렸습니다."), MEMBER_IS_NULL("회원이 존재하지 않습니다.")
 }
