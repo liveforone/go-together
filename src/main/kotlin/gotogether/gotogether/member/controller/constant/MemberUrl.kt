cfeffab1e6ddf5b9a548fa6b1c43ff1d61e030a4
liveforone <yc4852@gmail.com>
2023-06-26 16:17:22 +0900

    Add member url constant.

---
+++ src/main/kotlin/gotogether/gotogether/member/controller/constant/MemberUrl.kt
@@ -0,0 +1,11 @@
+package gotogether.gotogether.member.controller.constant
+
+object MemberUrl {
+    const val SIGNUP_MEMBER = "/member/signup"
+    const val SIGNUP_DRIVER = "/member/signup/driver"
+    const val LOGIN = "/member/login"
+    const val INFO = "/member/info"
+    const val UPDATE_PASSWORD = "/member/update/password"
+    const val UPDATE_BANKBOOK_NUM = "/member/update/bankbookNum"
+    const val PROHIBITION = "/prohibition"
+}
