ec0c8bb3384d4d7fd5c64d4385f73d1aa02f5dd3
liveforone <yc4852@gmail.com>
2023-06-26 14:42:14 +0900

    Add password util.

---
+++ src/main/kotlin/gotogether/gotogether/member/domain/util/PasswordUtil.kt
@@ -0,0 +1,11 @@
+package gotogether.gotogether.member.domain.util
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+object PasswordUtil {
+    private val passwordEncoder = BCryptPasswordEncoder()
+
+    fun encodePassword(password:String): String = passwordEncoder.encode(password)
+
+    fun isMatchPassword(password: String, originalPassword:String): Boolean = passwordEncoder.matches(password, originalPassword)
+}
